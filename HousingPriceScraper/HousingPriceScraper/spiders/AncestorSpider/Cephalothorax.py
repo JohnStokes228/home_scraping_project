0adc6ee726ec4f48f516686d9bc73da071cb752c
John Stokes <johnclstokes@hotmail.com>
2020-10-23 21:25:09 +0100

    implemented selenium driver in full basic, including scrolling and button pressing functions. wrote dummy quotes spider as proof of concept

--- HousingPriceScraper/HousingPriceScraper/spiders/AncestorSpider/Cephalothorax.py
+++ HousingPriceScraper/HousingPriceScraper/spiders/AncestorSpider/Cephalothorax.py
@@ -2,11 +2,11 @@
 includes Selenium features, to allow the spider to 'see' the website and, y'know, poke about on it I guess
 
 TODO - write basic interactivity methods
-     - * write replacements for all the existing scrapy / beautifulsoup4 methods. there are defs written for
-       each already *
+     - * write scrape cell to attribute method replacement *
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from random import randint
 import time
 import os
@@ -30,29 +30,29 @@ class PerceptiveAncestorSpider(AncestorSpider):
         self.driver.get(response.url)
         time.sleep(randint(2, 6))
 
-    def driver_get_element(self, target_xpath, multiple=True, xpath=True):
+    def driver_get_element(self, target_path, xpath=True, multiple=True):
         """
         selenium integrated version of abdomens 'get_element' method
 
-        :param target_xpath: xpath of target element
+        :param target_path: xpath of target element
         :param multiple: boolean indicating if expect multiple elements or if only one
         :param xpath: boolean set False to use CSS
-        :return: element
+        :return: list element
         """
         time.sleep(randint(2, 6))
         if not multiple:
             if xpath:
-                element = self.driver.find_element(By.XPATH, target_xpath)
+                element = self.driver.find_element(By.XPATH, target_path)
             else:
-                element = self.driver.find_element(By.CSS_SELECTOR, target_xpath)
+                element = self.driver.find_element(By.CSS_SELECTOR, target_path)
         else:
             if xpath:
-                element = self.driver.find_elements(By.XPATH, target_xpath)
+                element = self.driver.find_elements(By.XPATH, target_path)
             else:
-                element = self.driver.find_elements(By.CSS_SELECTOR, target_xpath)
+                element = self.driver.find_elements(By.CSS_SELECTOR, target_path)
         return element
 
-    def driver_element_to_attribute(self, element_list, attribute):
+    def driver_elements_to_attribute(self, element_list, attribute):
         """
         selenium integrated version of function converts list of elements to equal length list of the
         desired attribute.
@@ -61,27 +61,40 @@ class PerceptiveAncestorSpider(AncestorSpider):
         :param attribute: desired attribute to pull from each element
         :return: list of desired attributes
         """
+        if isinstance(element_list, list):
+            attribute_list = []
+            for element in element_list:
+                if attribute == 'text':
+                    attribute_list.append(element.text)
+                else:
+                    attribute_list.append(element.get_attribute(attribute))
+            return attribute_list
+        else:
+            if attribute == 'text':
+                attribute = element_list.text
+            else:
+                attribute = element_list.get_attribute(attribute)
+            return attribute
 
-        pass
-
-    def driver_scrape_to_attribute(self, target_path, attribute, multiple=True, xpath=True):
+    def driver_scrape_to_attribute(self, target_path, attribute, xpath=True, multiple=True):
         """
         selenium integrated function runs driver_element_to_attribute(driver_get_element())
 
-        :param response: scrapy response object - the target url
         :param target_path: the xpath / css selector of the desired element
         :param attribute: the desired attribute i.e. href, a, etc...
+        :param multiple: boolean indicating if the scrape is singular or multiple
         :param xpath: boolean indicating if target_path is xpath or css selector. defaults to xpath
         :return: list of attributes
         """
 
-        pass
+        elements = self.driver_get_element(target_path, multiple, xpath)
+        attributes = self.driver_elements_to_attribute(elements, attribute)
+        return attributes
 
-    def driver_scrape_table_to_dict(self, response, var_col, var_attr, val_col, val_attr, xpath=True):
+    def driver_scrape_table_to_dict(self, var_col, var_attr, val_col, val_attr, xpath=True):
         """
         selenium integrated function to scrape a table of data into a dictionary
 
-        :param response: scrapy response object
         :param var_col: column of table containing variable names
         :param var_attr: attribute for var_col path
         :param val_col: column of table containing variable realisations
@@ -90,34 +103,93 @@ class PerceptiveAncestorSpider(AncestorSpider):
         :return: dictionary with keys = var_col and values = att_col
         """
 
-        pass
+        var_names = self.driver_scrape_to_attribute(var_col, var_attr, xpath)
+        vals = self.driver_scrape_to_attribute(val_col, val_attr, xpath)
+        table_dict = {var_names[i]: [vals[i]] for i in range(len(var_names))}
+        return table_dict
 
-    def driver_scrape_multiple_to_attribute(self, response, list_of_tuples):
+    def driver_scrape_multiple_to_attribute(self, list_of_tuples):
         """
         selenium integrated function that will scrape a list of tuples using the scrape_to_attribute function
 
-        :param response: scrapy response object - the url of the site
         :param list_of_tuples: tuples of form (var_name, target_path, attribute, xpath=True (optional))
         :return: dictionary of chosen attributes, whose keys are the var_names from the tuples.
         """
 
-        pass
+        output_dict = {}
+        for input_tuple in list_of_tuples:
+            output_dict[input_tuple[0]] = self.driver_scrape_to_attribute(*input_tuple[1:])
+        return output_dict
 
-    def driver_scrape_product_box(self, response, product_path, attribute_lists_list, xpath=True):
+    def driver_scrape_product_box(self, product_path, attribute_lists_list, xpath=True, multiple=True):
         """
         selenium integrated scrape whole product cell from shelf level webpage. using this method should
         avoid any data alignment issues, by instead using Beautiful Soup to pull the attributes from specific
-        products after first scraping all html related to said product.
+        products after first scraping all html related to said product. code might need to be tweaked as its currently
+        the exact code i used to write this method for ONS...
 
-        :param response: scrapy response object - the url of the site
         :param product_path: xpath or css selector pointing to the product cell
-        :param attribute_lists_list: list of lists of form (var_name, node, node_index, attribute, condition (optional))
-               condition must be a dictionary of form {attribute: attribute_value}
+        :param attribute_lists_list: list of lists of form (var_name, xpath, attribute), where xpath starts with a
+               single / and can be indexed using [] starting from 1.
         :param xpath: boolean indicating if product_path is using xpath or css
+        :param multiple: boolean indicating if there are multiple elements cells to scrape
         :return: dictionary of chosen attributes, whose keys are the var_names from attribute_tuples_list
         """
 
-        pass
+        output_dict = {var_name: [] for var_name in [tup[0] for tup in attribute_lists_list]}
+        product_cell_list = self.driver_get_element(product_path, xpath, multiple)
+        for target_element in attribute_lists_list:
+            for cell in product_cell_list:
+                attrs = cell.find_elements_by_xpath('.{}'.format(target_element[1]))
+                attrs = self.driver_elements_to_attribute(attrs, target_element[2])
+                if 0 < len(attrs) < 1:
+                    output_dict[target_element[0]].append(attrs)
+                elif len(attrs) == 0:
+                    output_dict[target_element[0]].append('')
+                else:
+                    output_dict[target_element[0]].append(attrs[0])
+        return output_dict
+
+    def find_press_button(self, xpath):
+        """
+        find a button via xpath and then click it.
+
+        :param xpath: xpath for the button. I see no need for css to be an option ever here
+        :return: the button is clicked you hear me!! but legit though nothing is returned here calm down pls
+        """
+        try:
+            button = self.driver_get_element(xpath, multiple=False)
+            time.sleep(randint(1, 3))
+            try:
+                button.click()
+                return True
+            except:
+                print('button not clickable!')
+                return False
+        except NoSuchElementException:
+            print('button not found!')
+            return False
+
+    def scroll_to_bottom(self, number_of_stops=3):
+        """
+        selenium driver function that will scroll down to the bottom of a webpage, in number_of_stops unique intervals.
+        this function is built to deal with infinite scrolls, as well as pages that require sight on cells to load the
+        content.
+
+        :param number_of_stops: an integer dictating the number of stops along the way you hit whilst scrolling.
+        :return: WebDriver will scroll down a page until it finds an element
+        """
+        current_bottom = self.driver.execute_script("return document.body.scrollHeight")
+        keep_scrolling = True
+        while keep_scrolling:
+            for i in range(0, number_of_stops):
+                self.driver.execute_script("window.scrollTo({}, {});".format((i*(current_bottom/number_of_stops)), ((i+1)*(current_bottom/number_of_stops))))
+                time.sleep(randint(1, 3))
+            new_bottom = self.driver.execute_script("return document.body.scrollHeight")
+            if new_bottom == current_bottom:
+                keep_scrolling = False
+            else:
+                current_bottom = new_bottom
 
     def close(spider, reason):
         """
--- HousingPriceScraper/HousingPriceScraper/spiders/DummySpiders/DummyQuotes.py
+++ HousingPriceScraper/HousingPriceScraper/spiders/DummySpiders/DummyQuotes.py
@@ -2,10 +2,7 @@
 file containing Dummy Quotes spider. I will test all future changes on this spider. includes immutable scrape,
 addition of expanded list variables.
 
-TODO - write some kind of attribute method to test how multiple drivers works as I think its possible
-       that there might be some messiness involved with that. it could just use the one parents driver
-       but it needs to be tested to be sure.
-
+TODO 
 """
 from HousingPriceScraper.HousingPriceScraper.spiders.AncestorSpider.Cephalothorax import PerceptiveAncestorSpider
 from HousingPriceScraper.HousingPriceScraper.functions.data_management import save_dict_to_json, expand_list_variable
@@ -16,11 +13,15 @@ class DummyQuotesBaseSpider(PerceptiveAncestorSpider):
     def get_items(self, response):
 
         self.get_url(response)
-        elements = [("quote", "//div/span[@class='text']", "text"),
-                    ("author", "//div/span/small", "text"),
-                    ("tags", "//div/meta", "content")]
-        test = self.scrape_multiple_to_attribute(response, elements)
-        self.driver_get_element("//div/span[@class='text']")
-        test['page_order'] = list(range(len(test['quote'])))
-        test = expand_list_variable(test, 'tags', delete_old_var=False)
-        save_dict_to_json(test, self.data_path, self.name)
+        while True:
+            elements = [("quote", "//div/span[@class='text']", "text"),
+                        ("author", "//div/span/small", "text"),
+                        ("tags", "//div/meta", "content")]
+            test = self.driver_scrape_multiple_to_attribute(elements)
+            test['page_order'] = list(range(len(test['quote'])))
+            test = expand_list_variable(test, 'tags', delete_old_var=False)
+            save_dict_to_json(test, self.data_path, self.name)
+            self.scroll_to_bottom()
+            btn = self.find_press_button("//li[@class='next']/a[@href]")
+            if not btn:
+                break
